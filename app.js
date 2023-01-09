5d6fa31768566bb5497263ac491e90599dbbb007
stagnant-sys <113386819+stagnant-sys@users.noreply.github.com>
2023-01-09 22:19:51 +0100

    Finish working version of project
    - Finish styling all the content using Grid
    - Add Javascript for quick feedback on clicking 'Favorite' buttons
    - Add visual feedback on hover on multiple divs

---
+++ app.js
@@ -0,0 +1,20 @@
+let starButton = document.querySelectorAll('.star-button');
+let icon = document.querySelectorAll('.icon');
+const MODAL = document.querySelector('.star-modal');
+
+function openModal() {
+  MODAL.classList.add('open-modal');
+  setTimeout(() => {
+    MODAL.classList.remove('open-modal');
+  }, 4000)
+}
+
+for (let i = 0; i < starButton.length; i++) {
+  starButton[i].addEventListener('click', openModal);
+}
+
+for (let i = 0; i < starButton.length; i++) {
+  starButton[i].addEventListener('click', function() {
+    starButton[i].src = './images/star-icon-clicked.svg'
+  })
+}
